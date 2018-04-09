aea96e4fbcbf95fd5d4b96bba50850c19177d17c
jader_tao <jader_tao@qq.com>
2018-04-09 23:42:09 +0800

    dev: config app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { BoardComponent } from './board/board.component';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StreamComponent } from './stream/stream.component';
+import { AuthGuardService } from './service/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  }, {
+    path: '',
+    component: BoardComponent,
+    canActivate: [AuthGuardService],
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
