c2b48d2e5841067b949e5d3838d5a8ce08002714
jader_tao <jader_tao@qq.com>
2018-04-09 23:43:42 +0800

    dev: log in page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,71 @@
+import { AuthService } from './../service/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { validateCallback } from '@firebase/util';
+
+@Component({
+  selector: 'luv-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+
+  private email = '';
+  private password = '';
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  resolveInput(e: HTMLInputElement) {
+    console.log(e.value);
+    const value = e.value;
+    switch (e.value) {
+      case 'login':
+        e.value = 'logging in...';
+        console.log(this.email, this.password);
+        if (this.email && this.password) {
+          this.auth
+            .login(this.email, this.password)
+            .then(() => {
+              this.router.navigate(['/']);
+            })
+            .catch((error) => {
+              this.reset();
+              e.value = `${error.code} ${error.message}`;
+            });
+        } else {
+          e.value = 'incompleted message';
+          this.reset();
+          setTimeout(() => {
+            e.value = '';
+          }, 3000);
+        }
+        break;
+      case 'clear':
+        this.reset();
+        localStorage.removeItem('user');
+        e.value = '';
+        break;
+      default:
+        if (!this.email) {
+          this.email = value;
+          e.value = '';
+        } else if (this.email && !this.password) {
+          this.password = value;
+          e.value = '';
+        } else {
+          console.log(this.email, this.password);
+        }
+        break;
+    }
+  }
+
+  reset() {
+    this.email = '';
+    this.password = '';
+  }
+
+  ngOnInit() {
+  }
+
+}
