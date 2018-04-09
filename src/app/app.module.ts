0f1f09a30ec31587a8bacb9ad2477b28f8ac0b00
jader_tao <jader_tao@qq.com>
2018-04-09 23:43:06 +0800

    dev: adjust page arrangement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { StreamComponent } from './stream/stream.component';
+import { LoginComponent } from './login/login.component';
+import { BoardComponent } from './board/board.component';
+import { AuthGuardService } from './service/auth-guard.service';
+import { AuthService } from './service/auth.service';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StreamComponent
-],
+    StreamComponent,
+    LoginComponent,
+    BoardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [AuthGuardService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/utils/interface.ts
@@ -0,0 +1,7 @@
+export interface Luvletter {
+  id?: number;
+  user: string;
+  createTime: number;
+  content: string;
+  mood: string;
+}
