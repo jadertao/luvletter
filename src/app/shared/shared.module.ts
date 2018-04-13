30b92b9d690b79ed84948e193df92c6d49af93e4
jader_tao <jader_tao@qq.com>
2018-04-13 23:50:36 +0800

    dev: top loading bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PostitemComponent } from './post/postitem/postitem.component';
 import { EditorComponent } from './editor/editor.component';
 import { ButtonComponent } from './button/button.component';
+import { LoadingBarComponent } from './loading-bar/loading-bar.component';
 
 @NgModule({
   imports: [
@@ -14,11 +15,13 @@ import { ButtonComponent } from './button/button.component';
     PostlistComponent,
     EditorComponent,
     ButtonComponent,
+    LoadingBarComponent,
 ],
   exports: [
     PostlistComponent,
     EditorComponent,
     ButtonComponent,
+    LoadingBarComponent,
   ]
 })
 export class SharedModule { }
