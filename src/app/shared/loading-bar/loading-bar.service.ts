20c358389f9478353728e497f4fe420041c3c94e
jader_tao <jader_tao@qq.com>
2018-04-17 23:23:46 +0800

    add: loading bar component

---
+++ src/app/shared/loading-bar/loading-bar.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+
+export enum SlimLoadingBarEventType {
+  VISIBLE
+}
+
+export class SlimLoadingBarEvent {
+  constructor(public type: SlimLoadingBarEventType, public value: any) { }
+}
+
+@Injectable()
+export class LoadingBarService {
+
+  public _visible = false;
+
+  private eventSource: Subject<SlimLoadingBarEvent> = new Subject<SlimLoadingBarEvent>();
+  public events: Observable<SlimLoadingBarEvent> = this.eventSource.asObservable();
+
+
+  set visible(value: boolean) {
+    this._visible = value;
+    this.emitEvent(new SlimLoadingBarEvent(SlimLoadingBarEventType.VISIBLE, this._visible));
+    console.log(value);
+  }
+
+  get visible(): boolean {
+    return this._visible;
+  }
+
+  constructor() { }
+
+  private emitEvent(event: SlimLoadingBarEvent) {
+    if (this.eventSource) {
+      // Push up a new event
+      this.eventSource.next(event);
+    }
+  }
+
+  show() {
+    this.visible = true;
+  }
+
+  hide() {
+    this.visible = false;
+    console.log('hide');
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { PostitemComponent } from './post/postitem/postitem.component';
 import { EditorComponent } from './editor/editor.component';
 import { ButtonComponent } from './button/button.component';
 import { LoadingBarComponent } from './loading-bar/loading-bar.component';
+import { LoadingBarService } from './loading-bar/loading-bar.service';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
   imports: [
@@ -16,12 +18,17 @@ import { LoadingBarComponent } from './loading-bar/loading-bar.component';
     EditorComponent,
     ButtonComponent,
     LoadingBarComponent,
-],
+    ScrollToTopComponent,
+  ],
+  providers: [
+    LoadingBarService,
+  ],
   exports: [
     PostlistComponent,
     EditorComponent,
     ButtonComponent,
     LoadingBarComponent,
+    ScrollToTopComponent,
   ]
 })
 export class SharedModule { }
