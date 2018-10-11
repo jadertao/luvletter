e0ff0e4206c10c09b5daec536042bf0ec9b69441
jader_tao <jader_tao@qq.com>
2018-10-11 22:22:51 +0800

    refactor(ui-library): use ng-zorro instead of material

--- src/app/letter-board/letter-board.module.ts
+++ src/app/letter-board/letter-board.module.ts
@@ -7,12 +7,17 @@ import { MaterialModule } from '../shared/material/material.module';
 import { RouterModule } from '@angular/router';
 import { LetterPostComponent } from './letter-post/letter-post.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     RouterModule,
     ReactiveFormsModule,
+    NgZorroAntdModule
   ],
   declarations: [
     LetterBoardComponent,
@@ -28,6 +33,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     LetterCardComponent,
     LetterListComponent,
     LetterPostComponent,
-  ]
+  ],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
 })
 export class LetterBoardModule { }
