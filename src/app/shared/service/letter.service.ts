b74b6d4b20847b852f754914534817dfae7b3aba
jader_tao <jader_tao@qq.com>
2018-11-01 17:17:02 +0800

    dev(letter): subject

--- src/app/shared/service/letter.service.ts
+++ src/app/shared/service/letter.service.ts
@@ -11,68 +11,122 @@ export class LetterService {
 
   content = 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
     + ' A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.';
-  letters: Luvletter[] = [{
-    id: 1,
-    account: 'foo',
-    nickname: 'bar',
-    createTime: 1536114907125,
-    content: this.content,
-    mood: 'happy',
-    tags: ['默认'],
-  }, {
-    id: 2,
-    account: 'foo',
-    nickname: 'bar',
-    createTime: 1536114907125,
-    content: this.content,
-    mood: 'happy',
-    tags: ['默认'],
-  }, {
-    id: 3,
-    account: 'foo',
-    nickname: 'bar',
-    createTime: 1536114907125,
-    content: this.content,
-    mood: 'happy',
-    tags: ['默认'],
-  }, {
-    id: 4,
-    account: 'foo',
-    nickname: 'bar',
-    createTime: 1536114907125,
-    content: this.content,
-    mood: 'happy',
-    tags: ['默认'],
-  }];
+  letters: Luvletter[];
+  lettersSubject: Subject<Luvletter[]>;
   modal: boolean;
   state: Subject<string>;
   constructor(
     private auth: AuthService
   ) {
     this.modal = false;
-    this.state = new Subject();
-    this.state.next('done');
+    this.state = new Subject<string>();
+
+    this.lettersSubject = new Subject<Luvletter[]>();
+    this.letters = [{
+      id: 1,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }, {
+      id: 2,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }, {
+      id: 3,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }, {
+      id: 4,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }, {
+      id: 5,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }, {
+      id: 6,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }, {
+      id: 7,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }, {
+      id: 8,
+      account: 'foo',
+      nickname: 'bar',
+      createTime: 1536114907125,
+      content: this.content,
+      mood: 'happy',
+      tags: ['默认'],
+    }];
+
+    // this.state.next('done');
+    // this.lettersSubject.next(this.letters);
   }
 
-  getLetters(): Luvletter[] {
-    return this.letters;
+  getState() {
+    return this.state.asObservable();
   }
+
+  broadCastState(state: string) {
+    this.state.next(state);
+  }
+
+  broadCastLetters(letters: Luvletter[]) {
+    this.lettersSubject.next(letters);
+  }
+
+  getLetters() {
+    return this.lettersSubject.asObservable();
+  }
+
   post(rawLetter: any) {
     const timestamp = new Date().getTime();
     const id = this.letters.length + 1;
     const nickname = this.letters[0].nickname;
     const account = this.auth.user.account;
     const letter = Object.assign({}, rawLetter, { timestamp, id, nickname, account });
-    console.log(letter);
-    this.letters.unshift(letter);
+
+    this.letters = [letter, ...this.letters];
+
+    this.broadCastLetters(this.letters);
     this.toggleState('init');
     this.click();
   }
+
   click() {
-    window.setTimeout(() => this.toggleState('done'), 300);
+    window.setTimeout(() => this.broadCastState('done'), 300);
   }
+
   toggleState(s: string) {
-    console.log(s);
-    this.state.next(s);
+    this.broadCastState(s);
   }
 }
