e53f63b2bf36dde8710d722e7c4c367c4d6fcce9
jader_tao <jader_tao@qq.com>
2018-11-21 23:50:49 +0800

    dev(http): add http module

---
+++ src/app/http/intercepter/auth.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import dlv from 'dlv';
+import { AuthService } from '../../shared/service/auth/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private auth: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+
+    const authToken = dlv(this.auth.user, 'token');
+
+    if (authToken) {
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${authToken}`)
+      });
+      return next.handle(authReq);
+    }
+
+    return next.handle(req);
+  }
+}
---
+++ src/app/http/url.ts
@@ -0,0 +1,7 @@
+import { environment } from '../../environments/environment';
+
+const { prefix } = environment;
+console.log(`${prefix}/login`);
+export const LOGIN = `${prefix}/login`;
+export const LENGTH_OF_PAGES = `${prefix}/letter/length`;
+export const ALL_LETTERS = `${prefix}/letter`;
