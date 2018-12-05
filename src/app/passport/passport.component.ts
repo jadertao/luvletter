f28f2ec17c49a7a36fbe5e1d33e62af3459d47c2
jader_tao <jader_tao@qq.com>
2018-12-05 22:43:23 +0800

    dev(passport): add some content and do media query

--- src/app/passport/passport.component.ts
+++ src/app/passport/passport.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AVATAR } from '../http/url';
+import { getValueOfUserInfo } from '../utils/ls';
+import { Router } from '@angular/router';
+import { AccountInfo } from '../utils/interface';
 
 @Component({
   selector: 'luv-passport',
@@ -7,7 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PassportComponent implements OnInit {
 
-  constructor() { }
+
+  public accountInfo: Partial<AccountInfo>;
+  constructor(private router: Router) { }
+
+  public makeUpAvatar = AVATAR;
+  public getUserInfo = getValueOfUserInfo(this.router);
+  public cacheUserAccountInfo(): Partial<AccountInfo> {
+    if (this.accountInfo) {
+      return this.accountInfo;
+    }
+    this.accountInfo = getValueOfUserInfo(this.router)();
+    return this.accountInfo;
+  }
 
   ngOnInit() {
   }
